ea606858e23741d4eb55d2d8248e29aea7a93038
Andre-luiz41 <andreluandrade35@gmail.com>
2025-05-03 10:21:23 -0300

    Remove all files from master

--- backend/AuthBackend/src/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-const API_BASE_URL = "http://localhost:5065/api"; // Para desenvolvimento local
-// Ou, se você já tiver o backend em produção:
-// const API_BASE_URL = "https://seu-backend.com/api";
-
-export { API_BASE_URL };
--- frontend/src/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-// API URL - URL do backend no Render
-export const API_URL = 'https://auth-react-app-production.up.railway.app/'; // URL do backend implantado no Render
-
-// Local storage key for JWT token
-export const AUTH_TOKEN_KEY = 'authToken';
